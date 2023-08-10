902f6b503f2d5b96d457d6f002d5f2f476c99ef6
Leniors <mokualeeroy@gmail.com>
2023-08-10 11:01:41 -0700

    commadline arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-from sys import argv
-length = len(argv)
-if length == 1:
-    print("{} arguments.".format(length - 1))
-elif length == 2:
-    print("{} argument:".format(length - 1))
-    print("{}: {}".format(length - 1, argv[length - 1]))
-elif length > 2:
-    print("{} arguments:".format(length - 1))
-    for i in range(1, length):
-        print("{}: {}".format(i, argv[i]))
+if __name__ == "__main__":
+    from sys import argv
+    length = len(argv)
+    if length == 1:
+        print("{} arguments.".format(length - 1))
+    elif length == 2:
+        print("{} argument:".format(length - 1))
+        print("{}: {}".format(length - 1, argv[length - 1]))
+    elif length > 2:
+        print("{} arguments:".format(length - 1))
+        for i in range(1, length):
+            print("{}: {}".format(i, argv[i]))
