dc923202c2269a1aeed3f30acdc96a39d79bb08a
Leniors <mokualeeroy@gmail.com>
2023-08-10 09:42:15 -0700

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+def main():
     import add_0
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add_0.add(a, b)))
 
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add_0.add(a, b)))
+if __name__ == "__main__":
+    main()
