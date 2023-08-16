945522e69b71c1645e1570fabc491e0be9f0808f
Leniors <mokualeeroy@gmail.com>
2023-08-16 12:08:12 -0700

    roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,27 +1,21 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    my_list = []
-    if not roman_string:
-        return (0)
-    for j in range(len(roman_string)):
-        if roman_string[j] == "M" and roman_string[j - 1] != "C":
-            my_list.append(1000)
-        elif roman_string[j] == "X" and roman_string[j - 1] == "I":
-            my_list.append(8)
-        elif roman_string[j] == "C" and roman_string[j - 1] == "X":
-            my_list.append(80)
-        elif roman_string[j] == "M" and roman_string[j - 1] == "C":
-            my_list.append(800)
-        elif roman_string[j] == "I":
-            my_list.append(1)
-        elif roman_string[j] == "V":
-            my_list.append(5)
-        elif roman_string[j] == "X":
-            my_list.append(10)
-        elif roman_string[j] == "L":
-            my_list.append(50)
-        elif roman_string[j] == "C":
-            my_list.append(100)
-        elif roman_string[j] == "D":
-            my_list.append(500)
-    return sum(my_list)
+    total = 0
+    prev = 0
+    char = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+            }
+    for i in reversed(roman_string):
+        value = char[i]
+        if value >= prev:
+            total += value
+        else:
+            total -= value
+        prev = value
+    return total
