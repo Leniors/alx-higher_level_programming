354a495fc45144411d41018745a9b2e36699c349
Leniors <mokualeeroy@gmail.com>
2023-08-16 10:40:50 -0700

    roman to int

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    my_list = []
+    if not roman_string:
+        return (0)
+    for j in range(len(roman_string)):
+        if roman_string[j] == "M" and roman_string[j - 1] != "C":
+            my_list.append(1000)
+        elif roman_string[j] == "X" and roman_string[j - 1] == "I":
+            my_list.append(9)
+        elif roman_string[j] == "C" and roman_string[j - 1] == "X":
+            my_list.append(90)
+        elif roman_string[j] == "M" and roman_string[j - 1] == "C":
+            my_list.append(900)
+        elif roman_string[j] == "I":
+            my_list.append(1)
+        elif roman_string[j] == "V":
+            my_list.append(5)
+        elif roman_string[j] == "X" and roman_string[j - 1] != "I":
+            my_list.append(10)
+        elif roman_string[j] == "L":
+            my_list.append(50)
+        elif roman_string[j] == "C" and roman_string[j - 1] != "X":
+            my_list.append(100)
+        elif roman_string[j] == "D":
+            my_list.append(500)
+    return sum(my_list)
