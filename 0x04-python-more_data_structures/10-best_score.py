82e73952acab43d2ae42ded472e48f1cb17fcf88
Leniors <mokualeeroy@gmail.com>
2023-08-16 04:47:55 -0700

    best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,14 @@
+#!usr/bin/python3
+def best_score(a_dictionary):
+    minimum = 0
+    if not a_dictionary:
+        return
+    for i in a_dictionary:
+        minimum = a_dictionary.get(i)
+        break
+    for i in a_dictionary:
+        n = a_dictionary.get(i)
+        if n > minimum:
+            minimum = n
+            maximum = i
+    return maximum
