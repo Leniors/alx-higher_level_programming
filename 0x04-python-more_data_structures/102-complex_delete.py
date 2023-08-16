95891406a02e209789f3d750d77f048350ab7245
Leniors <mokualeeroy@gmail.com>
2023-08-16 11:21:46 -0700

    complex del

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    my_list = []
+    if not a_dictionary:
+        return
+    for i in a_dictionary:
+        if a_dictionary[i] == value:
+            my_list.append(i)
+    for i in my_list:
+        del a_dictionary[i]
+    return a_dictionary
