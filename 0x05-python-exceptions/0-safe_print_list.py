ff3199d289dd9a04d3075e95b8cca095dbddb343
Leniors <mokualeeroy@gmail.com>
2023-08-28 12:47:53 +0300

    print list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    try:
+        for element in my_list:
+            if count < x:
+                print(element, end='')
+                count += 1
+    except IndexError:
+        pass
+    finally:
+        print()
+        return count
