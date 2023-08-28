63f6b090cc5a7b2ae0ff1da39ec536c1723c17e5
Leniors <mokualeeroy@gmail.com>
2023-08-28 15:33:17 +0300

    handling function errors

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    x = None
+    try:
+        x = fct(*args)
+        return x
+    except Exception as err:
+        print("Exception: {}".format(err), file=sys.stderr)
+        return x
