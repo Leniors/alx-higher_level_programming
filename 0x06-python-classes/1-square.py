0c917d48e34517b70cee4c4544754f32e434fa73
Leniors <mokualeeroy@gmail.com>
2023-08-29 11:47:11 +0300

    private class attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+class Square:
+    """
+    Square - a square is a shape with four sides that have equal length
+    """
+    def __init__(self, size):
+        self.__size = size
