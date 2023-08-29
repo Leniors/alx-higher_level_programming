cae33318f87aba13a49ad25fcc320b7d3d1eef70
Leniors <mokualeeroy@gmail.com>
2023-08-29 12:33:59 +0300

    handle class exceptions

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,15 +6,8 @@ class Square:
     """Rep a class Square"""
     def __init__(self, size=0):
         """initialize the class"""
-        try:
-            if isinstance(size, int) is False:
-                raise TypeError
-            elif size < 0:
-                raise ValueError
-            self.__size = size
-        except TypeError:
-            print("size must be an integer")
-            return
-        except ValueError:
-            print("size must be >= 0")
-            return
+        if isinstance(size, int) is False:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
