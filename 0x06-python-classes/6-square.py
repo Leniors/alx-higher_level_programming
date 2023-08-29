6b2015ff2bee2946d7fe96e114376b469a2cc15c
Leniors <mokualeeroy@gmail.com>
2023-08-29 19:28:02 +0300

    position of square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -51,6 +51,9 @@ class Square:
         if self.__size == 0:
             print()
         else:
+            if self.__position[1] > 0:
+                for i in range(self.__position[1]):
+                    print()
             for i in range(self.__size):
                 for x in range(self.__position[0]):
                     print(" ", end="")
