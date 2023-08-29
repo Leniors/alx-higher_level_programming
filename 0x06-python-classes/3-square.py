820fe7651deba9248752beb79732fe8498205401
Leniors <mokualeeroy@gmail.com>
2023-08-29 12:45:29 +0300

    area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -11,7 +11,7 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-    
+
     def area(self):
         """get area of square"""
         return self.__size * self.__size
