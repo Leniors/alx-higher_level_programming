a0a34490f138efb65d30a436cc7194358a30e182
Leniors <mokualeeroy@gmail.com>
2023-09-11 09:20:50 +0300

    list attributes and methods

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+defining a function that returns a list of available attributes and methods
+"""
+
+
+def lookup(obj):
+    """implimenting the function"""
+    return dir(obj)
