2b0e660c0c7e2da281e0d7316c21633bd497d8f8
Leniors <mokualeeroy@gmail.com>
2023-09-11 10:02:22 +0300

    is instance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+creating a function that compares two objects and see if they are the same
+"""
+
+
+def is_same_class(obj, a_class):
+    """implimenting the function"""
+    return isinstance(obj, a_class)
