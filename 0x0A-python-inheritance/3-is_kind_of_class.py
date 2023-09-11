0e78913e8fd7285f9ef7797475da6998a03892b8
Leniors <mokualeeroy@gmail.com>
2023-09-11 18:42:08 +0300

    is instance

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -5,6 +5,7 @@ define function that checks for instance or subclass
 
 
 def is_kind_of_class(obj, a_class):
+    """implimenting the function"""
     if isinstance(obj, a_class):
         return True
     else:
