6403bad056ec9a9b64c9df86c4abef5625033621
Leniors <mokualeeroy@gmail.com>
2023-09-12 23:53:44 +0300

    json

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""class"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            # If attrs is not specified, retrieve all attributes
+            return self.__dict__
+
+        # Initialize an empty dictionary to store the selected attributes
+        selected_attrs = {}
+
+        # Loop through the attrs list and only include valid attributes
+        for attr in attrs:
+            if hasattr(self, attr):
+                selected_attrs[attr] = getattr(self, attr)
+
+        return selected_attrs
