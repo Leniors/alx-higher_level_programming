b94c056183af2a8f11cf8f87fb9f3b480616e12d
Leniors <mokualeeroy@gmail.com>
2023-11-15 00:04:29 +0300

    reverse

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   let last = list.length - 1;
-  for (let i = 0; i < list.length  && i <= last; i++) {
+  for (let i = 0; i < list.length && i <= last; i++) {
     const temp = list[i];
     list[i] = list[last];
     list[last] = temp;
