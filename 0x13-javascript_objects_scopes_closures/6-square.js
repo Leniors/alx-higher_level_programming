dd2300bdcf392070fbaabe77e5aa515a09f372c1
Leniors <mokualeeroy@gmail.com>
2023-11-14 23:36:27 +0300

    other method

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,16 +3,22 @@ const Square1 = require('./5-square');
 
 module.exports = class Square extends Square1 {
   charPrint (c) {
-    if (c != undefined) {
+    if (c !== undefined) {
       for (let i = 0; i < this.height; i++) {
         let line = '';
         for (let j = 0; j < this.width; j++) {
-          line += 'c';
+          line += 'C';
         }
         console.log(line);
       }
     } else {
-      this.print();
+      for (let i = 0; i < this.height; i++) {
+        let line = '';
+        for (let j = 0; j < this.width; j++) {
+          line += 'X';
+        }
+        console.log(line);
+      }
     }
   }
 };
