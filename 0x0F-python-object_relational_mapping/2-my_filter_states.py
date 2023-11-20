f4a44d49bd526b72ca260eabd17032e016a6147a
Leniors <mokualeeroy@gmail.com>
2023-11-20 03:44:01 +0300

    list some

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """only execute when specified"""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    name = sys.argv[3]
+    searched = sys.argv[4]
+    db = MySQLdb.connect(
+            user=username,
+            passwd=password,
+            db=name,
+            host='localhost',
+            port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id")
+    rows = cur.fetchall()
+    for row in rows:
+        if row[1] == searched:
+            print(row)
+    cur.close()
+    db.close()
