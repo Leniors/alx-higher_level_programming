a1ac2a6bade8e2fa5fdaec376ae2c25088f11544
Leniors <mokualeeroy@gmail.com>
2023-11-20 03:32:50 +0300

    list

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
+    """only execute when specified"""
     if len(sys.argv) != 4:
         sys.exit()
     username = sys.argv[1]
