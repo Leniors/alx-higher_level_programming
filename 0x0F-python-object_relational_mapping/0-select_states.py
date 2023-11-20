6b55044386a0113f8a7863c155745f82f3b83709
Leniors <mokualeeroy@gmail.com>
2023-11-20 03:18:03 +0300

    list

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,7 @@ if len(sys.argv) != 4:
 username = sys.argv[1]
 password = sys.argv[2]
 name = sys.argv[3]
-db = MySQLdb.connect(user=username, passwd=password, db=name, hostname='localhost', port=3306)
+db = MySQLdb.connect(user=username, passwd=password, db=name, host='localhost', port=3306)
 cur = db.cursor()
 cur.execute("SELECT * FROM states ORDER BY states.id")
 rows = cur.fetchall()
