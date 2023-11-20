ecef31025e4ffb8e58ed9de7d7a6d8ec28405a7a
Leniors <mokualeeroy@gmail.com>
2023-11-20 04:59:32 +0300

    alias tables

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""list all cities in the hbtn_0e_4_usa database"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """executes only if the fie is called"""
+    if len(sys.argv) != 4:
+        sys.exit()
+    username = sys.argv[1]
+    password = sys.argv[2]
+    name = sys.argv[3]
+    db = MySQLdb.connect(
+        user=username,
+        passwd=password,
+        db=name,
+        host='localhost',
+        port=3306)
+    cur = db.cursor()
+    query = """
+    SELECT cities.id, cities.name, states.name AS state_name
+    FROM cities
+    INNER JOIN states ON cities.state_id = states.id
+    ORDER BY cities.id
+    """
+    cur.execute(query)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
