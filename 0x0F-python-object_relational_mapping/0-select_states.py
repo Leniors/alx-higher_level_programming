bbcf3883b75c06d010735f403554ec0253c18a0b
Leniors <mokualeeroy@gmail.com>
2023-11-20 03:03:46 +0300

    MYSQLdb

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 import MySQLdb
+import sys
 
-db = MySQLdb.connect(user='root', passwd='root', db='hbtn_0e_0_usa')
+if len(sys.argv) != 4:
+    sys.exit()
+username = argv[1]
+password = argv[2]
+name = argv[3]
+db = MySQLdb.connect(user=username, passwd=password, db=name)
 cur = db.cursor()
 cur.execute("SELECT * FROM hbtn_0e_0_usa.states ORDER BY id")
 rows = cur.fetchall()
