2393cf3aef388fbd7645ab7312ce25962db3604c
Leniors <mokualeeroy@gmail.com>
2023-11-23 00:21:35 +0300

    get info

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -4,10 +4,12 @@ from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine(f'mysql://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}')
-Session = sessionmaker(bind=engine)
-session = Session()
-all_states = session.query(State).order_by(State.id).all()
-for state in all_states:
-    print(f"{state.id}: {state.name}")
-session.close()
+if __name__ == '__main__':
+    engine = create_engine(f'mysql://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    all_states = session.query(State).order_by(State.id).all()
+
+    for state in all_states:
+        print(f"{state.id}: {state.name}")
+    session.close()
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,11 +1,12 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
+if __name__ == '__main__':
+    Base = declarative_base()
 
-class State(Base):
-    """Class State"""
-    __tablename__ = 'states'
+    class State(Base):
+        """Class State"""
+        __tablename__ = 'states'
 
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
-    name = Column(String(128), nullable=False)
+        id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+        name = Column(String(128), nullable=False)
