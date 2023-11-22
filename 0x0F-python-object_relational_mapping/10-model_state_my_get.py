10a15d52604ab560b449b42b5e2014184404aac4
Leniors <mokualeeroy@gmail.com>
2023-11-23 00:59:30 +0300

    find

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    """only when called"""
+    if len(sys.argv) != 5:
+        sys.exit()
+    engine = create_engine(f'mysql://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    search_name = sys.argv[4]
+    state = session.query(State).filter(State.name == search_name).first()
+
+    if state:
+        print(f"{state.id}")
+    else:
+        print("Not found")
