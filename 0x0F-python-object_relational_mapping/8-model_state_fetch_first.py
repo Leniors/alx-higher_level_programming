d6826f0ab47ea89c990327eabff3221cbfcd962c
Leniors <mokualeeroy@gmail.com>
2023-11-23 00:36:35 +0300

    first

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    """only when called"""
+    if len(sys.argv) != 4:
+        sys.exit()
+    engine = create_engine(f'mysql://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    first_state = session.query(State).order_by(State.id).first()
+
+    if first_state:
+        print(f"{first_state.id}: {first_state.name}")
+    else:
+        print("Nothing")
