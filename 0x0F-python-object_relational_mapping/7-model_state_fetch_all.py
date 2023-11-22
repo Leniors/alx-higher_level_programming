0050d437356b013af3936499360da2fed7625f6c
Leniors <mokualeeroy@gmail.com>
2023-11-23 00:23:37 +0300

    get info

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -5,6 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == '__main__':
+    """only when called"""
     engine = create_engine(f'mysql://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}')
     Session = sessionmaker(bind=engine)
     session = Session()
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 if __name__ == '__main__':
+    """only when called"""
     Base = declarative_base()
 
     class State(Base):
