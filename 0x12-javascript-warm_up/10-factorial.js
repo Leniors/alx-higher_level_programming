439d6157710f32dac9d52fa02187495e2f9e795b
Leniors <mokualeeroy@gmail.com>
2024-01-08 17:41:24 +0300

    Recursion

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+function recursion(n) {
+	if (isNaN(Number(n))) {
+		return 1;
+	} else if (n === 0) {
+		return 1;
+	}
+	return n * recursion(n - 1);
+}
+console.log(recursion(process.argv[2]));
