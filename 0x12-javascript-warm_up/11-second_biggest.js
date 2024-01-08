c61fbdae4d172b1270244bef9be8f4764c2a5e44
Leniors <mokualeeroy@gmail.com>
2024-01-09 00:33:18 +0300

    Second Biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
-if ((process.argv).length <= 2) {
-	console.log(0);
-	return;
-} else if ((process.argv).length === 3) {
-	console.log(0);
-	return;
+if (process.argv.length <= 2) {
+  console.log(0);
+  return;
+} else if (process.argv.length === 3) {
+  console.log(0);
+  return;
 }
 let myArray = [];
-for (let i = 2; i < (process.argv).length; i++) {
-	myArray.push(process.argv[i]);
+for (let i = 2; i < process.argv.length; i++) {
+  myArray.push(process.argv[i]);
 }
 myArray.sort(function (a, b) {
-	return a - b;
-})
+  return a - b;
+});
 let len = myArray.length;
 console.log(myArray[len - 2]);
