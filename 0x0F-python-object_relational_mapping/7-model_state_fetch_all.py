2c1eaa2915e67f1ac7e98075286d7bdf08cdffe0
Leniors <mokualeeroy@gmail.com>
2024-01-13 23:59:11 +0300

    List states

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,7 +10,8 @@ if __name__ == '__main__':
     """only when called"""
     if len(sys.argv) != 4:
         sys.exit()
-    engine = create_engine(f'mysql://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}')
+    engine = create_engine(
+        f'mysql://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}')
     Session = sessionmaker(bind=engine)
     session = Session()
     all_states = session.query(State).order_by(State.id).all()
