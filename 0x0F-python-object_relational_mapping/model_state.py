950ddfc5ed468ad4ef4ca9384c4aa3ba56d7b07d
Leniors <mokualeeroy@gmail.com>
2024-01-13 20:37:13 +0300

    model state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""model sstates
+"""
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
