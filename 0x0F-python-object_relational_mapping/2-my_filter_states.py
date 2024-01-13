02a61e49852f29984921cb066fb3d17bbbfbf693
Leniors <mokualeeroy@gmail.com>
2024-01-14 02:34:31 +0300

    filter states

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""lists all states from the database hbtn_0e_0_usa"""
+"""lists all states from the database hbtn_0e_0_usa
+"""
 import MySQLdb
-import sys
+imprt sys
 
 if __name__ == "__main__":
     """only execute when specified"""
@@ -12,13 +13,14 @@ if __name__ == "__main__":
     name = sys.argv[3]
     searched = sys.argv[4]
     db = MySQLdb.connect(
-            user=username,
-            passwd=password,
-            db=name,
-            host='localhost',
-            port=3306)
+        user=username,
+        passwd=password,
+        db=name,
+        host='localhost',
+        port=3306)
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC".format(searched)
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC".format(
+        searched)
     cur.execute(query)
     rows = cur.fetchall()
     for row in rows:
