e57914319274ee036dd7e01694dfe96702b8ac61
Leniors <mokualeeroy@gmail.com>
2024-01-14 00:13:25 +0300

    Sate with a)

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""States with letter a
+"""
 import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
@@ -8,10 +10,13 @@ if __name__ == '__main__':
     """only when called"""
     if len(sys.argv) != 4:
         sys.exit()
-    engine = create_engine(f'mysql://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}')
+    engine = create_engine(
+        f'mysql://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}')
     Session = sessionmaker(bind=engine)
     session = Session()
-    states_with_a = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+    states_with_a = session.query(State).filter(
+        State.name.like('%a%')).order_by(
+        State.id).all()
 
     for state in states_with_a:
         print(f"{state.id}: {state.name}")
