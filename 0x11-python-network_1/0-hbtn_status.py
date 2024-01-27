a7c1a8725e080e86249d51ce949ca16fd20341d5
Leniors <mokualeeroy@gmail.com>
2024-01-28 02:21:08 +0300

    status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-"""fetch url
-"""
-
+"""fetch url"""
 import urllib.request
 
-if __name__ == "__main__":
-    """work on execution only"""
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        content = response.read()
-        print("Body response:")
-        print("\t- type:", type(content))
-        print("\t- content:", content)
-        print("\t- utf8 content:", content.decode('utf-8'))
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    content = response.read()
+    print("Body response:")
+    print("\t- type:", type(content))
+    print("\t- content:", content)
+    print("\t- utf8 content:", content.decode('utf-8'))
