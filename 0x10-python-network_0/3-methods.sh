92329f0a996aac70565626afc65ca4b87c7b8a2e
Leniors <mokualeeroy@gmail.com>
2024-01-27 15:14:56 +0300

    options

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Display all HTTP methods accepted by the servera
-curl -sI -X OPTIONS $1 | grep -i "Allow" | awk '{print $2}'
+curl -sI -X OPTIONS $1 | grep -i "Allow:" | awk '{print substr($0, 8)}'
