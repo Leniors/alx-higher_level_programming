f49559fe3173e2fc3d64b155f660b09029f0033d
Leniors <mokualeeroy@gmail.com>
2024-01-28 02:25:59 +0300

    header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,8 +3,7 @@
 import urllib.request
 import sys
 
-url = sys.argv[1]
-
-with urllib.request.urlopen(url) as response:
+with urllib.request.urlopen(sys.argv[1]) as response:
+    """open url"""
     respo_id = response.getheader('X-Request-Id')
     print(respo_id)
