44c4d0252ef95536957409f37552e6e2a6bfa44e
Leniors <mokualeeroy@gmail.com>
2024-01-27 15:34:32 +0300

    body

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # body echo -n "${response:0:$((${#response}-4))}"
-response=$(curl -s -w "%{http_code}" $1) && [ "${response: -3}" == "200" ] && echo -n "$(curl -s $1)"
+response=$(curl -s -o /dev/null -w "%{http_code}" $1) && [ $response == 200 ] && echo -n "$(curl -s $1)"
