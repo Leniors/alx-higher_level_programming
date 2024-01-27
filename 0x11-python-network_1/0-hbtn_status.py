a9ac85ef72561448a6488802788cd718c4a5f6e2
Leniors <mokualeeroy@gmail.com>
2024-01-28 02:16:17 +0300

    status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import urllib.request
 """fetch url"""
+import urllib.request
+
 if __name__ == "__main__":
     """work on execution only"""
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
