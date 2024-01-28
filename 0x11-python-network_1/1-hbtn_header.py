c066780fc8ebc7dcdd69c0e993f23141b3d19066
Leniors <mokualeeroy@gmail.com>
2024-01-28 13:34:51 +0300

    header value

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""header"""
+"""value of 'X-Request-Id' in header"""
 import sys
 import urllib.request
 
 with urllib.request.urlopen(sys.argv[1]) as response:
     """open url"""
     head = response.headers
-    print(head.get('X-Request-Id'))
+    req_id = head.get('X-Request-Id')
+
+print(req_id)
