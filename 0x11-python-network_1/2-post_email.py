0cc2b2da8ec01b56dcbd79e51fb4729d8e2ca080
Leniors <mokualeeroy@gmail.com>
2024-01-28 03:16:10 +0300

    post

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""post"""
+import sys
+import urllib.request
+import urllib.parse
+
+url = sys.argv[1]
+values = {'email': sys.argv[2]}
+data = urllib.parse.urlencode(values)
+data = data.encode('ascii') # data should be bytes
+req = urllib.request.Request(url, data)
+
+with urllib.request.urlopen(req) as response:
+    reply = response.read()
+    print(reply.decode('utf-8'))
