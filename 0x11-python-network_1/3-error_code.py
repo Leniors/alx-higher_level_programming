fb9ebd01985910ef69a3f0319755820a3a8134f0
Leniors <mokualeeroy@gmail.com>
2024-01-29 20:46:43 +0300

    HTTP error

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,11 +2,11 @@
 """HTTPError"""
 import sys
 import urllib.request
-import urllib.error.HTTPError
+import urllib.error
 
 try:
     with urllib.request.urlopen(sys.argv[1]) as response:
         content = response.read()
         print(content.decode('utf-8'))
 except urllib.error.HTTPError as e:
-    print(e.code)
+    print(f"Error code: {e.code}")
