d2d09e78b6250cea817bd436549d96bfcf9ab0d1
Leniors <mokualeeroy@gmail.com>
2024-02-20 23:49:59 +0300

    api

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const request = require('request');
 const [, , id] = process.argv;
-const url = `https://swapi-api.alx-tools.com/api/films/${id}`
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 request(url, (error, response) => {
   if (error) {
     return;
   }
-  const obj_res = JSON.parse(response.body)
-  console.log(obj_res.title);
+  const obj = JSON.parse(response.body);
+  console.log(obj.title);
 });
